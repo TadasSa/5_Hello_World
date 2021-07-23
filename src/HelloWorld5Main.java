32a668cb3227a8b69b7986b226f398d81e770f0f
TadasSa <t.sabaitis@gmail.com>
2021-07-23 09:29:30 +0300

    initial commit

---
+++ src/HelloWorld5Main.java
@@ -0,0 +1,14 @@
+public class HelloWorld5Main {
+    public static void main(String[] args) {
+        printHelloWorld();
+    }
+
+    public static void printHelloWorld() {
+        System.out.println(getHelloWorld());
+    }
+
+    public static String getHelloWorld() {
+        return "Hello World !!!!!";
+    }
+
+}
